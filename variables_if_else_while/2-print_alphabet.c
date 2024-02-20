1d4a29c2869e463961536357b19a68227f829705
ninotna <antonin@dev87.fr>
2024-02-20 15:09:49 +0100

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function main random
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int car; /* car for character*/
+
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		putchar(car);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
