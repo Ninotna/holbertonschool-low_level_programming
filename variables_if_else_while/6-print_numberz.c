688fd485c08d86388af07974437013eb29212f81
ninotna <antonin@dev87.fr>
2024-02-20 16:07:43 +0100

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function main random
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
+
