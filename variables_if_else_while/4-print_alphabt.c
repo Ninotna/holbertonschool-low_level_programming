054d121e897faafb1dddf054a0fd2631b522e9c4
ninotna <antonin@dev87.fr>
2024-02-20 15:30:08 +0100

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function main random
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int car; /* car for character*/
+
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		{
+			if (car != 'q' || car != 'e')
+			{
+				putchar(car);
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
