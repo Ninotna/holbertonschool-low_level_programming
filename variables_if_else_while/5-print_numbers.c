febd37873904aba4a29301f534a64946200314e7
ninotna <antonin@dev87.fr>
2024-02-20 16:00:29 +0100

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function main random
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	while (digit < 10)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
