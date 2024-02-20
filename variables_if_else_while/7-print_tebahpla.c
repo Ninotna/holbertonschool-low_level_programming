8382af8a827b5a647d13275b26f94d6c4e624c1a
ninotna <antonin@dev87.fr>
2024-02-20 18:53:08 +0100

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function main random
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int car;
+
+	for (car = 'z'; car > 'a'; car--)
+	{
+		putchar(car);
+	}
+	putchar('\n');
+	return (0);
+}
