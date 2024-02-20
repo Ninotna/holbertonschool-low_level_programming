d74eff9dc18781796630a4705413f0a896a6b024
ninotna <antonin@dev87.fr>
2024-02-20 19:44:10 +0100

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	char digits[] = "0123456789abcdef";
+	int i;
 
-	for (int i = 0; i < 10; ++i)
+	for (i = 0; i < 10; ++i)
 	{
 		putchar(digits[i]);
 		putchar('\n');
 	}
 
-	for (int i = 0; i < 16; ++i)
+	for (i = 0; i < 16; ++i)
 	{
 		putchar('0');
 		putchar(digits[i]);
