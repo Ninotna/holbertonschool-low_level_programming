8b8f9caa0c097e00bdf12a8e9ca0a39a670fbdd2
ninotna <antonin@dev87.fr>
2024-02-21 16:43:30 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char = 'a'; char <= 'z'; char++)
+	{
+	print_alphabet(char);
+	}
+	puchar('\n');
+	return (0);
+}
