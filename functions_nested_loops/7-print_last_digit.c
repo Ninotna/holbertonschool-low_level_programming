1d09c07dbb22dc3c4a17e879fdeeff8ba557cdad
ninotna <antonin@dev87.fr>
2024-02-21 18:13:37 +0100

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function that prints the last digit of a number.
+ * @i : integer
+ * Return: last digit
+ */
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+	return (i);
+}
+
+
