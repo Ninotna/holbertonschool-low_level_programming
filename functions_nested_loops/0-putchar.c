862acbdcc4fc5e2b03f5a2ab3f45af58b766441b
ninotna <antonin@dev87.fr>
2024-02-21 16:20:48 +0100

    nouveau commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,17 @@
-#include <main.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype de la fonction _putchar */
+int _putchar(char c);
+
+/* Prototypes des autres fonctions */
+int function1(int arg1, char arg2);
+void function2(void);
+double function3(double arg1, double arg2);
+
+/* Ajoutez d'autres prototypes de fonctions ici */
+
+#endif /* MAIN_H */
 
 /**
  * main -  prints _putchar, followed by a new line
