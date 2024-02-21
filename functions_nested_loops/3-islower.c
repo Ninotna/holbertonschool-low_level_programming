aa2788e5ba9470f591aa08632f3525501cab70ae
ninotna <antonin@dev87.fr>
2024-02-21 17:24:41 +0100

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character
+ * @c : character
+ * Return: 1 (lower case) or 0
+ */
+
+int _islower(int c)
+{
+
+(c == 1) ? Returns(1) : Returns(0);
+
+}
+
