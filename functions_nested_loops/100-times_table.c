6be480f904e30f541d9641e131daf55cb042e36b
ninotna <antonin@dev87.fr>
2024-02-23 17:50:15 +0100

    100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 			for (col = 0; col <= n; col++)
 			{
 				int product = row * col;
-				/* Vérifier si le produit a un ou deux chiffres */
+
 				if (product >= 100)
 				{
 					_putchar(product / 100 + '0'); /* Affiche le chiffre des centaines */
@@ -27,21 +27,16 @@ void print_times_table(int n)
 				{
 					_putchar(product / 10 + '0'); /* Affiche le chiffre des dizaines */
 					_putchar(product % 10 + '0'); /* Affiche le chiffre des unités */
+					_putchar(' '); /* Pour aligner les produits à 1 et 2 chiffres */
 				}
 				else
 				{
 					if (col != 0)
 					{
-						_putchar(' '); /* Pour aligner les produits à 1 et 2 chiffres */
+						_putchar(','); /* Pour aligner les produits à 1 et 2 chiffres */
 						_putchar(' '); /* Pour aligner les produits à 1 et 2 chiffres */
 					}
 					_putchar(product + '0');
-				}
-				/* Espacer les colonnes avec ', ' */
-				if (col < n)
-				{ /* pas de ', ' après le dernier de la ligne */
-					_putchar(',');
-					_putchar(' ');
 				}
 			}
 			_putchar('\n');
