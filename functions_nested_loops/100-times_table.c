8c1e0661e3fe2a45178f5c05e1409c652fa1c229
ninotna <antonin@dev87.fr>
2024-02-23 17:30:14 +0100

    100-times_table.c

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * print_times_table - function that prints the n times table, starting with 0.
+ * @n : n tables
+ * Return: n times table
+ */
+
+void print_times_table(int n)
+{
+	int row, col;
+
+	if (n < 15 || n > 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				int product = row * col;
+				/* Vérifier si le produit a un ou deux chiffres */
+				if (product >= 10)
+				{
+					putchar(product / 10 + '0'); /* Affiche le chiffre des dizaines */
+					putchar(product % 10 + '0'); /* Affiche le chiffre des unités */
+				}
+				else
+				{
+					if (col != 0)
+					{
+						putchar(' '); /* Pour aligner les produits à 1 et 2 chiffres */
+					}
+					putchar(product + '0');
+				}
+				/* Espacer les colonnes avec ', ' */
+				if (col < n)
+				{ /* pas de ', ' après le dernier de la ligne */
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			putchar('\n');
+		}
+	}
+}
+
+
