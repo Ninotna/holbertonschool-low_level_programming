a4ded7d721f9873c935d7a2dd8344f170bc7c924
ninotna <antonin@dev87.fr>
2024-02-23 15:46:57 +0100

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,23 +3,11 @@
  * add - function that adds two integers and returns the result.
  * @a : integer
  * @b : b
- * Return: 0
+ * Return: addi a + b
  */
 int add(int a, int b)
 {
 	int addi = a + b;
-	int res;
 
-		if (addi >= 10)
-		{
-			res = _putchar(addi / 10 + '0'); /* Affiche le chiffre des dizaines */
-			res += _putchar(addi % 10 + '0'); /* Affiche le chiffre des unités */
-		}
-		else
-		{
-			res = _putchar(addi + '0');
-		}
-
-	_putchar('\n');
-	return ();
+	return (addi);
 }
