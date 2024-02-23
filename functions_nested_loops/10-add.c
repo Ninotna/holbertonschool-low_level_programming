9968cda3462c24ab2ef6f5ef6baf82694d2647d6
ninotna <antonin@dev87.fr>
2024-02-23 13:31:20 +0100

    main.h

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * @a : integer
+ * @b : b
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	int a, b;
+
+	int addi = a + b;
+
+		if (addi >= 10)
+		{
+			_putchar(addi / 10 + '0'); /* Affiche le chiffre des dizaines */
+			_putchar(addi % 10 + '0'); /* Affiche le chiffre des unités */
+		} else
+		{
+			_putchar(addi + '0');
+		}
+
+	_putchar('\n');
+}
+
