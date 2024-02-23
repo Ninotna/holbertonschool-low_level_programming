abdaa16c350696c645bf7e1d3244c049e756774c
ninotna <antonin@dev87.fr>
2024-02-23 16:14:54 +0100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,11 @@ void print_to_98(int n)
 {
 	for (n ; n <= 98 ; n++)
 	{
-		printf("%d",n);
+		printf("%d", n);
 			if (n < 98)
 			{
-				printf(", ")
+				printf(", ");
 			}
 	}
-
-	if (n == 98)
 	putchar('\n');
-
 }
