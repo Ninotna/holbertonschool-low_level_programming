0f4fd2d2bd8f8a1a76d4d303f6ad0f259b7711d6
ninotna <antonin@dev87.fr>
2024-02-26 17:23:41 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,13 +7,13 @@
 
 void print_diagonal(int n)
 {
-	while (n >= 0)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else if (n > 0)
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
 		{
 			for (; n > 0; n--)
 			{
@@ -24,4 +24,3 @@ void print_diagonal(int n)
 		}
 	}
 }
-
