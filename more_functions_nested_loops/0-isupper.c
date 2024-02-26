d7d1fc31003f949b94ba4dc5e061cd3b3c7a0f7d
ninotna <antonin@dev87.fr>
2024-02-26 13:44:55 +0100

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
-/** 
- * _isupper - a function that checks for uppercase character.
- * @car :
- * return: 1 or 0 
-*/
+/**
+ * _isupper - function that checks for uppercase character.
+ * @car : integer
+ * Return: 1 or 0
+ */
 
-int _isupper (int car) 
+int _isupper(int car)
 {
-
-    if (car >= 'A' && car <= 'Z')
-        {
-            return (1);
-        }
-    else 
-        {
-            return (0);
-        }
+	if (car >= 'A' && car <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
