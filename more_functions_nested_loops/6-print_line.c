16574f8a8c7537e93838fa2de576ce5a437b210a
ninotna <antonin@dev87.fr>
2024-02-26 15:46:13 +0100

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,19 @@
 
 void print_line(int n)
 {
-	while (n >= 0)
 	{
-		_putchar('_');
 		if (n <= 0)
 		{
 			_putchar('\n');
 		}
-		n--;
+		else
+		{
+			while (n > 0)
+			{
+				_putchar('_');
+				n--;
+			}
+		}
 	}
 }
 
