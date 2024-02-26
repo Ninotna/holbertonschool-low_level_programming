8026baef2a4a6f583acb1a964ac7727b2b83a2f2
ninotna <antonin@dev87.fr>
2024-02-26 15:07:02 +0100

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints 10 times the numbers, from 0 to 14, followed by \\n
+ * @n: number of _
+ * Return: print most numbers
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	while (i > 0)
+	{
+		_putchar('95');
+		(i <= 0) ? _putchar('\n') : i--;
+	}
+
+}
+
