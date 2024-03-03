98a641d445a91d269e2463f86af73495947c4504
ninotna <antonin@dev87.fr>
2024-03-03 17:30:59 +0100

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,34 +10,34 @@
 
 int _atoi(char *s)
 {
-    int i = 0;
-    int result = 0;
-    int sign = 1; /* Initialize sign to positive */
+	int i = 0;
+	int result = 0;
+	int sign = 1; /* Initialize sign to positive */
 
-    /* Skip leading whitespace */
-    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
-    {
-        i++;
-    }
+	/* Skip leading whitespace */
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+	{
+		i++;
+	}
 
-    /* Check for sign */
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[i] == '+')
-    {
-        i++;
-    }
+	/* Check for sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
 
-    /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	/* Convert digits to integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    /* Apply sign */
-    return sign * result;
+	/* Apply sign */
+	return (sign * result);
 }
