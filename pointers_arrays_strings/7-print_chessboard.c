aa6b1e8da0940469be4ef33ddf865bbbecdfe42e
ninotna <antonin@dev87.fr>
2024-03-06 10:09:41 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,21 +4,20 @@
 
 /**
  * print_chessboard - a function that prints the chessboard.
- * @sa: string to check
+ * @a: the board
  * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
+int i, j;
 
-int i,j;
-
-for (i=0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
-    for (j=0; j< 8; j++)
-    {
-        putchar(a[i][j]);
-    }
-    putchar('\n');
+	for (j = 0; j < 8; j++)
+	{
+		putchar(a[i][j]);
+	}
+	putchar('\n');
 }
 }
