84c5c8f5a07d34d74872a6710914c8f5a8cb4dee
ninotna <antonin@dev87.fr>
2024-03-06 11:06:00 +0100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.h>
-
-/**
- * _strncmp - compares two strings within b bytes.
- * @s1: string to check
- * @s2: string to check from
- * @b: number of bytes to check
- * Return: 0 if strings are the same
- * any other number if they are different.
- */
-int _strncmp(char *s1, char *s2, unsigned int b)
-{
-	unsigned int i;
-	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
-	{
-		s1++;
-		s2++;
-	}
-	return (i - b);
-}
 
 /**
  * _strstr - a function that locates a substring.
@@ -30,13 +10,28 @@ int _strncmp(char *s1, char *s2, unsigned int b)
 
 char *_strstr(char *haystack, char *needle)
 {
+	if (!*needle)
+	{
+		return (haystack);
+	}
 
-    while(*haystack != '\0')
-    {
-        if (*haystack == *needle) 
-        {
-            return (haystack);
-        }
-    }
-    return ((*haystack == *needle) ? haystack : NULL);
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h && *n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (!*n)
+		{
+			return (haystack);
+		}
+	haystack++;
+	}
+
+return (NULL);
+
 }
