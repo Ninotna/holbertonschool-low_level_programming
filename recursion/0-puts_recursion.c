25824dfe97cca430c79bcbfa4cd396a0633e799d
ninotna <antonin@dev87.fr>
2024-03-06 15:47:54 +0100

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: pointer to the string to convert
+ * Return: the converted string.
+ */
+
+void _puts_recursion(char *s)
+{
+    int i = 0;
+
+while (s[i] != '\0')
+{
+    putchar(s[i]);
+i++;
+}
+}
