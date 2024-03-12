4f9db57e0621d727c1de1861a5f1a3bde1c1cc72
ninotna <antonin@dev87.fr>
2024-03-12 13:23:31 +0100

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,25 +14,30 @@
 char *_strdup(char *str)
 {
 	unsigned int i, len;
-	char *_str;
+	char *strbis;
 
-	len = strlen(str);
-
-	_str = (char *) malloc(len * sizeof(char));
-
-	if (str == NULL || _str == NULL)
+	if (str == NULL)
 	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != '\0'; len++)
+	;
+
+	strbis = (char *)malloc((len + 1) * sizeof(char));
+	/* +1 for null terminator */
+
+	if (strbis == NULL)
+	{
+		printf("Erreur : allocation de mémoire a échoué.\n");
 		return (NULL);
 	}
 
 	for (i = 0; i < len; i++)
 	{
-		_str[i] = str[i];
+		strbis[i] = str[i];
 	}
-	_str[i] = '\0';
+	strbis[i] = '\0';
 
-	free(_str);
 
-	return (_str);
-
+	return (strbis);
 }
