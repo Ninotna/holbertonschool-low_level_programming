efcdb64fe2afde0b9a801f76092e7f9def851b70
ninotna <antonin@dev87.fr>
2024-03-12 16:55:03 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,10 +16,8 @@ char *str_concat(char *s1, char *s2)
 	int buffer;
 	char *res;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return ("");
-	}
+	(s1 == NULL) ? s1 = "" : "";
+	(s2 == NULL) ? s2 = "" : "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 	;
