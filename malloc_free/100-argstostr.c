ce2c0aa146aa8751b664307f12e5772c6f8a5ce2
ninotna <antonin@dev87.fr>
2024-03-13 15:26:36 +0100

    100-argstostr.c

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -6,43 +6,50 @@
 
 /**
 * argstostr - a function that concatenates all the arguments of your program.
-* @grid: existing grid
-* @height: height
+* @ac: Argc
+* @av: Argv
 * Return: an array of char with args
 */
 
-char *argstostr(int ac, char **av) {
+char *argstostr(int ac, char **av)
+{
 	int i, j, len, bufferlen;
 	char *p;
 
 	/* Validation des arguments d'entrée */
-	if (ac <= 0 || av == NULL) {
-		return NULL;
+	if (ac <= 0 || av == NULL)
+	{
+		return (NULL);
 	}
 
 	/* Calcul de la longueur totale nécessaire */
 	len = bufferlen = 0;
-	for (i = 0; i < ac; i++) {
-		for (j = 0; av[i][j] != '\0'; j++) {
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
 			len++;
 		}
 	}
 
 	/* Allouer de la mémoire pour la chaîne résultante */
 	p = (char *)malloc(len + ac + 1); /* +1 pour le '\0' final */
-	if (p == NULL) {
-		return NULL; /* Échec de l'allocation de mémoire */
+	if (p == NULL)
+	{
+		return (NULL); /* Échec de l'allocation de mémoire */
 	}
 
 	/* Concaténer les arguments */
 	bufferlen = 0;
-	for (i = 0; i < ac; i++) {
-		for (j = 0; av[i][j] != '\0'; j++) {
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
 			p[bufferlen++] = av[i][j];
 		}
 		p[bufferlen++] = '\n'; /* Ajouter un saut de ligne */
 	}
 	p[bufferlen] = '\0'; /* Terminer la chaîne avec un '\0' */
 
-	return p;
+	return (p);
 }
