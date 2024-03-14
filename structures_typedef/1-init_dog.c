eff142bfb85301addb4a47dfaf396e03e413d960
ninotna <antonin@dev87.fr>
2024-03-14 17:43:27 +0100

    init structures_typedef

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * init_dog - initialize struct dog.
+  * @d: struct name
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: name of owner of dog
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - desc
+ * @name: ff
+ * @age: ff
+ * @owner: fff
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
+
+
+
+
+
