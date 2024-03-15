b97a9a44ea00c67e4304d9bc4e3ffc4ea8730f36
ninotna <antonin@dev87.fr>
2024-03-15 10:07:08 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,19 +8,18 @@
  * @owner: fff
 */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
 #endif
-
 
 
 
