3a3aa474b971824f599e08d254822103ec89907e
ninotna <antonin@dev87.fr>
2024-03-15 20:05:44 +0100

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,52 +11,74 @@
 *Return: pointer to the alloc memory
 */
 
+#include <stdlib.h> /* For malloc */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n) 
 {
-    char *p;
-    unsigned int s1count, s2count, sizeBuffer, i;
 
-    // Use empty strings if NULL pointers are provided
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	char *p;
 
-    // Count the length of s1
-    for (s1count = 0; s1[s1count]; s1count++)
-        ;
+	unsigned int s1count, s2count, sizeBuffer, i;
 
-    // Count the length of s2
-    for (s2count = 0; s2[s2count]; s2count++)
-        ;
+	/* Initialize counters */
+	s1count = 0;
+	s2count = 0;
 
-    // Limit s2count to n if n is smaller
-    if (s2count > n) {
-        s2count = n;
-    }
+	/* Use empty strings if NULL pointers are provided */
+	if (s1 == NULL) 
+	{
+		s1 = "";
+	}
+	if (s2 == NULL) 
+	{
+		s2 = "";
+	}
 
-    // Calculate the buffer size (including space for the null terminator)
-    sizeBuffer = s1count + s2count + 1;
+	/* Count the length of s1 */
+	while (s1[s1count] != '\0') 
+	{
+		s1count++;
+	}
 
-    // Allocate memory for the new string
-    p = malloc(sizeBuffer * sizeof(char));
-    if (p == NULL) {
-        return (NULL);
-    }
+	/* Count the length of s2 */
+	while (s2[s2count] != '\0') 
+	{
+		s2count++;
+	}
 
-    // Copy s1 to the new string
-    for (i = 0; i < s1count; i++) {
-        p[i] = s1[i];
-    }
+	/* Limit s2count to n if n is smaller */
+	if (s2count > n) 
+	{
+		s2count = n;
+	}
 
-    // Concatenate n characters of s2 to the new string
-    for (i = 0; i < s2count; i++) {
-        p[i + s1count] = s2[i];
-    }
+	/* Calculate the buffer size (including space for the null terminator) */
+	sizeBuffer = s1count + s2count + 1;
 
-    // Null-terminate the new string
-    p[s1count + s2count] = '\0';
+	/* Allocate memory for the new string */
+	p = (char *)malloc(sizeBuffer * sizeof(char));
+	if (p == NULL) 
+	{
+		return (NULL);
+	}
 
-    return p;
+	/* Copy s1 to the new string */
+	for (i = 0; i < s1count; i++) 
+	{
+		p[i] = s1[i];
+	}
+
+	/* Concatenate n characters of s2 to the new string */
+	for (i = 0; i < s2count; i++) 
+	{
+		p[i + s1count] = s2[i];
+	}
+
+	/* Null-terminate the new string */
+	p[s1count + s2count] = '\0';
+
+	return (p);
 }
+
+
 
